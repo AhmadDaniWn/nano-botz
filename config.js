c9c9c3abb13dd9f28dba2e48e9ccbc144e84957e
AhmadDaniWn <jogetb548@gmail.com>
2023-08-04 02:39:48 +0700

    guy

---
+++ config.js
@@ -0,0 +1,98 @@
+// Script Gratis Jangan Dj Jual
+
+// - Ubah nomor owner dan wm di sini.
+// + Daftar apikey terlebih dahulu sebelum menggunakan svript ini link
+//https://api.betabotz.org
+//https://api.botcahx.live
+// - Jika sudah mendaftar salin key yang ada di profie dan paste di global.lann && global.btc
+// - Contoh global.btc = 'ngaksk?'//register di https://api.botcahx.live
+// - Contoh global.lann = 'Ntahla' //register key di https://api.betabotz.org
+// - Jika sudah maka lanjutkan untuk mengubah nomor owner dan wm.
+
+
+// List Key 
+// Betabotz = digunakan hampir di semua fitur kecuali linkshort
+// Botcahx  = digunakan untuk linkshort saja
+
+global.owner = ['6285892928715']  
+global.mods = ['6285892928715'] 
+global.prems = ['6285892928715']
+global.nameowner = 'Nano - Botz'
+global.numberowner = '6285892928715' 
+global.mail = 'nanobot@gmail.com' 
+global.dana = '085892928715'
+global.pulsa = '085892928715'
+global.gopay = '-'
+global.namebot = 'B16-MD'
+global.gc = 'https://chat.whatsapp.com/JLPXgJKp0eo7EC8Yknvn7J'
+global.web = 'https://youtube.com/@B16_OFC'
+global.instagram = 'https://instagram.com/kahfiofc'
+global.wm = 'B16-MD'
+global.watermark = wm
+global.wm2 = '⫹⫺ B16 Bot'
+global.wm3 = '© B16'
+global.wm4 = 'made by B16 Ofc'
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made with'
+global.author = 'Bot WhatsApp'
+
+//---- image thumbnail 
+global.thumb = 'https://telegra.ph/file/27178ffc0a142b4d0df5e.jpg'
+
+//===> Apikey
+global.lann = 'ypovXG6Z' //isi apikey mu https://api.betabotz.org
+global.btc = 'OiWYUqjm'//Isi apikey di https://api.botcahx.live
+global.APIs = {   
+  lann: 'https://api.betabotz.org',
+  btc: 'https://api.botcahx.live'
+}
+global.APIKeys = { 
+  'https://api.betabotz.org': 'ypovXG6Z' //isi apikey mu https://api.betabotz.org
+}
+
+global.multiplier = 45
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
